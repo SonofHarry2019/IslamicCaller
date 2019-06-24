32f92f3f6a849c8f6d8ffa6125ebb45287641e57
haaris <hhussain@xula.edu>
2019-06-24 12:47:00 -0500

    Initial commit

--- app/src/main/java/com/example/islamiccaller/MainActivity.java
+++ app/src/main/java/com/example/islamiccaller/MainActivity.java
@@ -9,12 +9,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Time t1 = new Time();
         System.out.println("The default number is " + t1);
-        Time t2 = new Time(-1234);
-        System.out.println("\nThe second default number is " + t2);
-        int hr = t2.getHour();
 
+        Time t2 = new Time(2234);
+        System.out.println("\nThe second default number is " + t2);
+
+        Time early = new Time(7890);
+        System.out.println(" ");
+
+        Time late = new Time(3);
+        System.out.println(" ");
+
+        int hr = t2.getHour();
         System.out.println("The hour for this particular time is " + hr);
     }
 }
